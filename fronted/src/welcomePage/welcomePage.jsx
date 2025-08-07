2304f34c576094ba1b6dd03f527392e9a07a475f
Jhon Deivid <jhondr10@hotmail.com>
2025-08-07 13:16:00 -0500

    v1.0 raffle, emptying, and winners determined, first beta version completed, code comments pending

--- fronted/src/welcomePage/welcomePage.jsx
+++ fronted/src/welcomePage/welcomePage.jsx
@@ -6,6 +6,8 @@ export function WelcomePage() {
 
   const [inputFriend, setInputFriend] = useState("");
 
+  const [winner, setWinner] = useState("");
+
   const radius = 250; //circle radius
 
   const handleChange = (e) => {
@@ -18,32 +20,65 @@ export function WelcomePage() {
 
   const handlePush = (e) => {
     if (inputFriend.trim() !== "") {
-      if (!arrayFriends.includes(inputFriend.trim())) {setArrysFriends((prev) => [...prev, inputFriend.trim()])}
-      else{return alert("Este amigo ya existe porfavor inserte uno nuevo")};
+      if (!arrayFriends.includes(inputFriend.trim())) {
+        setArrysFriends((prev) => [...prev, inputFriend.trim()]);
+      } else {
+        return alert("Este amigo ya existe porfavor inserte uno nuevo");
+      }
       setInputFriend("");
-    }else{
-      return alert("Porfavor inserte un nombre antes de continuar")
+    } else {
+      return alert("Porfavor inserte un nombre antes de continuar");
     }
+  };
+
+  const handleEmpty = (e) => {
+    setInputFriend("");
+    setArrysFriends([]);
+  };
+
+  const handleSort = (e) => {
+    const numFriendsWinner = Math.floor(Math.random() * arrayFriends.length);
+
+    setWinner(arrayFriends[numFriendsWinner]);
   };
 
   return (
     <div className="w-full h-auto flex justify-center items-center">
       <div className="relative w-[300px] h-screen rounded-full flex items-center justify-center">
         <div className="w-auto absolute flex flex-col items-center gap-2">
-          <img src={secretFriendImage} alt="" />
+          <img className="w-40 h-40" src={secretFriendImage} alt="" />
+          <h1 className="text-3xl font-semibold text-yellow-500 drop-shadow-lg tracking-wide text-center">
+            Escribe el nombre de un amigo
+          </h1>
           <input
             type="text"
             className="w-60 px-2 py-1 rounded-md border border-gray-400"
-            placeholder="Escribe el nombre de un amigo"
+            placeholder="Escribe un nombre"
             onChange={handleChange}
             value={inputFriend}
           />
-          <button
-            className="bg-[#33312B] text-white px-4 py-1 rounded-md transition-transform duration-300 ease-in-out  hover:bg-[#756A3B] hover:scale-125"
-            onClick={handlePush}
-          >
-            Enviar
-          </button>
+          <div className="w-60 flex flex-row place-content-between">
+            <button
+              className="bg-[#33312B] text-white px-4 py-1 rounded-md transition-transform duration-300 ease-in-out  hover:bg-[#756A3B] hover:scale-125"
+              onClick={handlePush}
+            >
+              Agregar
+            </button>
+            <button
+              className="bg-[#33312B] text-white px-4 py-1 rounded-md transition-transform duration-300 ease-in-out  hover:bg-[#756A3B] hover:scale-125"
+              onClick={handleEmpty}
+            >
+              Vaciar
+            </button>
+          </div>
+          {arrayFriends.length > 0 && (
+            <button
+              className="bg-[#33312B] text-white px-4 py-1 rounded-md transition-transform duration-300 ease-in-out  hover:bg-[#756A3B] hover:scale-125 z-40"
+              onClick={handleSort}
+            >
+              Sortear
+            </button>
+          )}
         </div>
 
         {arrayFriends?.map((friend, index) => {
@@ -62,6 +97,22 @@ export function WelcomePage() {
             </div>
           );
         })}
+
+        {winner && (
+          <div
+            className="fixed inset-0 bg-[rgba(0,0,0,0.5)] flex items-center justify-center z-50"
+            onClick={()=>setWinner("")}
+          >
+            <div className="bg-white p-6 rounded-lg shadow-lg w-96">
+              <h1 className="text-4xl font-semibold text-[#33312B] drop-shadow-lg tracking-wide text-center">
+                El ganador es
+                <p className="text-4xl font-semibold text-yellow-500 drop-shadow-lg tracking-wide text-center">
+                  {winner}
+                </p>
+              </h1>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
